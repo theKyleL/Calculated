6fd45bf9ea492e280c30410a2d276d675becb41b
Kyle <kyle.latino@gmail.com>
2017-08-16 22:54:23 -0400

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Calculated
+//
+//	A programmer friendly calculator application for iOS.
+//
+//  Created by Kyle Latino on 8/16/17.
+//  Copyright © 2017 Kyle Latino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
